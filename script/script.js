7957e4643654438f502b9ba1fbd3399dfabaee3a
Pedro Henrique Loriato <132790059+PedroLoriato@users.noreply.github.com>
2023-07-03 23:09:59 -0300

    Atualização do Site
    Atualizando página home, sendo adicionado a logo e favicon, e criação a página da aplicação, juntamente com uma aplicação para teste.

--- script/script.js
+++ script/script.js
@@ -1,7 +1,41 @@
 /*
 Descrição:
+Programação do site que está em desenvolvimento.
 
-
-Autores:
-Versão:
+Versão: 1.0 
 */
+
+const inBoi = document.getElementById("inBoi");
+const btCalcular = document.getElementById("btCalcular");
+const outPesqBoi = document.getElementById("outPesqBoi");
+
+btCalcular.addEventListener("click", calcularPeso);
+
+function calcularPeso() {
+
+    var indPesq = -1
+    var Boi = (inBoi.value).toUpperCase();
+
+    if (inBoi.value == "") {
+        alert = "Campo Vazio";
+        inBoi.focus();
+
+    } else {
+
+        for (let ind = 0; ind < vetID.length; ind++) {
+            if (vetID[ind] == Boi) {
+                indPesq = ind;
+            }
+        }
+        if (indPesq == -1) {
+
+            alert("Não foi encontrado a Indentificação do Boi!");
+            inBoi.value = "";
+            inBoi.focus();
+        }
+        else {
+            outPesqBoi.innerHTML = `O Boi com Identificador ${vetID[indPesq]}, chegou a Fazenda com Peso inicial de ${vetINI[indPesq]} arrobas,<br>
+                                    e ao final do Período de Engorda, ficou com um peso final de ${vetFinal[indPesq]} arrobas.  `
+        }
+    }
+}
